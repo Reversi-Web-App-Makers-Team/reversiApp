7c03e0bc33e5894eeaec6a5c53393641e9db4598
hirokinishimoto <shozemi.nishimoto@gmail.com>
2019-03-25 21:30:23 +0900

    mod: ページ遷移を実装

--- flask_demo.py
+++ flask_demo.py
@@ -13,7 +13,6 @@ from sqlite3_commands import INITIALIZE_BLACK_PLAYER_INFO
 from sqlite3_commands import GET_WHITE_PLAYER_BOARD
 from sqlite3_commands import UPDATE_BLACK_PLAYER_BOARD
 
-
 app = Flask(__name__)
 
 def get_db():
@@ -28,18 +27,34 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/home')
 def home():
     db = get_db()
     curs = db.cursor()
-    cursor.exexute(
+    cursor.exexute()
+    cursor.exexute()
+    cursor.exexute()
+    cursor.exexute()
+    return render_template('home.html')
 
-@app.route('/home/mode_select/')
+@app.route('/mode_select/')
 def mode_select(username=None):
     username = request.values['username']
     return render_template('mode_select.html', username=username)
 
-@app.route('/home/mode_select/game/'
+@app.route('/game/dqn/<index>/')
+def dqn():
+    return render_template('dqn.html', username=username)
+
+
+@app.route('/fin/')
+def fin():
+    return render_template('fin.html', winner="いより")
+
 
 def _main():
     app.run(debug=True)
